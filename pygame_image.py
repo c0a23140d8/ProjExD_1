aec6393d2697416951fc2e3fa35093b6d0510dcc
matsuoka <c0a23140d8@edu.teu.ac.jp>
2024-06-11 17:01:30 +0900

    演習2:move_ipメソッドを一つに下

--- pygame_image.py
+++ pygame_image.py
@@ -17,6 +17,8 @@ def main():
     kt_img = pg.transform.flip(kt_img, True, False)
     kt_rct = kt_img.get_rect()
     kt_rct.center = 300, 200
+    
+
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
@@ -27,14 +29,18 @@ def main():
         screen.blit(bg_img_tr, [-x+4800, 0])
         
         key_lst = pg.key.get_pressed()
+        sp = 0
+        y = 0
         if key_lst[pg.K_UP]:
-            kt_rct.move_ip((0 ,-1))
+            y = -1
         if key_lst[pg.K_DOWN]:
-            kt_rct.move_ip((0 ,1))
+            y = 1
         if key_lst[pg.K_RIGHT]:
             tmr += 1
         if key_lst[pg.K_LEFT]:
-            kt_rct.move_ip((-1 ,0))
+            sp = -1
+        kt_rct.move_ip((sp ,y))
+        
         screen.blit(kt_img, kt_rct)
         # screen.blit(kt_img, [300, 200])
         pg.display.update()
