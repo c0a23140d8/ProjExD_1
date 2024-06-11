6e87910ff845ff7c8866af4682d1f1f2601ac931
matsuoka <c0a23140d8@edu.teu.ac.jp>
2024-06-11 16:32:48 +0900

    7の手順:間延びの対処

--- pygame_image.py
+++ pygame_image.py
@@ -10,6 +10,7 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
+    bg_img_tr = pg.transform.flip(bg_img, True, False)
     tmr = 0
     
     kt_img = pg.image.load("fig/3.png")
@@ -17,8 +18,11 @@ def main():
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-
-        screen.blit(bg_img, [-(tmr%800), 0])
+        x = tmr%3200
+        screen.blit(bg_img, [-x, 0])
+        screen.blit(bg_img_tr, [-x+1600, 0])
+        screen.blit(bg_img, [-x+3200, 0])
+        screen.blit(bg_img_tr, [-x+4800, 0])
         
         kt_rct = kt_img.get_rect()
         kt_rct.center = 300, 200
